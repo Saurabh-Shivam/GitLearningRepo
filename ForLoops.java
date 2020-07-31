e5018cdf96695d61d3f5e5c5923f622ef19d7e1b
Saurabh <saurabhshivam4@gmail.com>
2020-07-31 17:37:36 +0530

    updated for loops with new messages

--- ForLoops.java
+++ ForLoops.java
@@ -1,7 +1,9 @@
 public class ForLoops {
 public static void main(String[] args) {	
 for(int i = 0; i < 10; i++) {
-System.out.println(" Hello World " + i); 
+System.out.println("Hello World" + i); 
+System.out.println("Done with for loops");
+System.out.println("Wait what about while and do while loops");
 }
 }
           
